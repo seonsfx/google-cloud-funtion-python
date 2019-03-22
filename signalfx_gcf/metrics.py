b1212f9fabe32f65e8132a975e735257ee25d1bf
Seon Kim <skim@signalfx.com>
2019-03-22 09:38:56 -0400

    Comment some lines

--- signalfx_gcf/metrics.py
+++ signalfx_gcf/metrics.py
@@ -55,10 +55,10 @@ def generate_wrapper_decorator(access_token):
             global ingest
             # timeout for connecting = 1 and responding 0.3
             ingest = sfx.ingest(access_token, timeout=(1, ingest_timeout))
-            context = args[1]  # expect context to be second argument
+            # context = args[1]  # expect context to be second argument
 
             global default_dimensions
-            default_dimensions.update(utils.get_fields(context))
+            # default_dimensions.update(utils.get_fields(context))
             default_dimensions['metric_source'] = 'gcf_wrapper'
 
             global is_cold_start
