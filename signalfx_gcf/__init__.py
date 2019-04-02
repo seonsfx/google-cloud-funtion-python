e839727f7045028dc317673cbe65d74e4e20c4f5
Seon Kim <skim@signalfx.com>
2019-04-02 11:22:59 -0400

    Unisolate

--- signalfx_gcf/__init__.py
+++ signalfx_gcf/__init__.py
@@ -1,41 +1,31 @@
-# import signalfx_serverless_common
+import signalfx_serverless_common
 
-from . import metrics
 from .version import name, version
 
-
+# backwards compatibility
 def wrapper(*args, **kwargs):
-    return metrics.wrapper(*args, **kwargs)
+    return signalfx_serverless_common.wrapper(*args, **kwargs)
 
 
 def emits_metrics(*args, **kwargs):
-    return metrics.wrapper(*args, **kwargs)
+    return signalfx_serverless_common.emits_metrics(*args, **kwargs)
 
 
-# # backwards compatibility
-# def wrapper(*args, **kwargs):
-#     return signalfx_serverless_common.wrapper(*args, **kwargs)
+def is_traced(*args, **kwargs):
+    return signalfx_serverless_common.is_traced(*args, **kwargs)
 
 
-# def emits_metrics(*args, **kwargs):
-#     return signalfx_serverless_common.emits_metrics(*args, **kwargs)
+# less convenient method
+def send_metric(counters=[], gauges=[]):
+    signalfx_serverless_common.send_metric(counters, gauges)
 
 
-# def is_traced(*args, **kwargs):
-#     return signalfx_serverless_common.is_traced(*args, **kwargs)
+# convenience method
+def send_counter(metric_name, metric_value=1, dimensions={}):
+    signalfx_serverless_common.send_counter(metric_name, metric_value, dimensions)
 
 
-# # less convenient method
-# def send_metric(counters=[], gauges=[]):
-#     signalfx_serverless_common.send_metric(counters, gauges)
-
-
-# # convenience method
-# def send_counter(metric_name, metric_value=1, dimensions={}):
-#     signalfx_serverless_common.send_counter(metric_name, metric_value, dimensions)
-
-
-# # convenience method
-# def send_gauge(metric_name, metric_value, dimensions={}):
-#     signalfx_serverless_common.send_gauge(metric_name, metric_value, dimensions)
+# convenience method
+def send_gauge(metric_name, metric_value, dimensions={}):
+    signalfx_serverless_common.send_gauge(metric_name, metric_value, dimensions)
 
