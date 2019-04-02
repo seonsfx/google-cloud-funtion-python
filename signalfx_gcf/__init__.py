bf34865b611d8906cbfa4b263a7518fe0cc6bef4
Seon Kim <skim@signalfx.com>
2019-04-02 10:21:09 -0400

    Isolate

--- signalfx_gcf/__init__.py
+++ signalfx_gcf/__init__.py
@@ -2,28 +2,34 @@ import signalfx_serverless_common
 
 # backwards compatibility
 def wrapper(*args, **kwargs):
-    return signalfx_serverless_common.wrapper(*args, **kwargs)
+    return True
+    # return signalfx_serverless_common.wrapper(*args, **kwargs)
 
 
 def emits_metrics(*args, **kwargs):
-    return signalfx_serverless_common.emits_metrics(*args, **kwargs)
+    return True
+    # return signalfx_serverless_common.emits_metrics(*args, **kwargs)
 
 
 def is_traced(*args, **kwargs):
-    return signalfx_serverless_common.is_traced(*args, **kwargs)
+    return True
+    # return signalfx_serverless_common.is_traced(*args, **kwargs)
 
 
 # less convenient method
 def send_metric(counters=[], gauges=[]):
-    signalfx_serverless_common.send_metric(counters, gauges)
+    return True
+    # signalfx_serverless_common.send_metric(counters, gauges)
 
 
 # convenience method
 def send_counter(metric_name, metric_value=1, dimensions={}):
-    signalfx_serverless_common.send_counter(metric_name, metric_value, dimensions)
+    return True
+    # signalfx_serverless_common.send_counter(metric_name, metric_value, dimensions)
 
 
 # convenience method
 def send_gauge(metric_name, metric_value, dimensions={}):
-    signalfx_serverless_common.send_gauge(metric_name, metric_value, dimensions)
+    return True
+    # signalfx_serverless_common.send_gauge(metric_name, metric_value, dimensions)
 
