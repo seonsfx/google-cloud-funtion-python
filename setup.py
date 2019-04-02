f5ebbee6be000a4dd3435663afba29ca74f7030a
Seon Kim <skim@signalfx.com>
2019-04-02 15:33:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages=find_packages(),
     install_requires=requirements,
     dependency_links=[
-        'git+https://github.com/seonsfx/serverless-common-python.git#egg=signalfx_serverless_common-0.1.1.1'
+        'https://github.com/seonsfx/signalfx_serverless_common/tarball/master#egg=signalfx_serverless_common-0.1.1.1'
     ],
     classifiers=[
         'Operating System :: OS Independent',
