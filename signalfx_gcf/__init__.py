81e016a15ec811282e1384b4951b0942f6fe9569
Seon Kim <skim@signalfx.com>
2019-04-02 13:43:51 -0400

    Update __init__.py

--- signalfx_gcf/__init__.py
+++ signalfx_gcf/__init__.py
@@ -1,36 +1,36 @@
-from .signalfx_serverless_common import common
+from signalfx_gcf.signalfx_serverless_common import signalfx_serverless_common
 
 from . import utils
 from .version import name, version
 
 def __init():
     fields = utils.get_fields()
-    common.configure('gcf', fields)
+    signalfx_serverless_common.configure('gcf', fields)
 
 # backwards compatibility
 def wrapper(*args, **kwargs):
-    return common.wrapper(*args, **kwargs)
+    return signalfx_serverless_common.wrapper(*args, **kwargs)
 
 
 def emits_metrics(*args, **kwargs):
-    return common.emits_metrics(*args, **kwargs)
+    return signalfx_serverless_common.emits_metrics(*args, **kwargs)
 
 
 def is_traced(*args, **kwargs):
-    return common.is_traced(*args, **kwargs)
+    return signalfx_serverless_common.is_traced(*args, **kwargs)
 
 
 # less convenient method
 def send_metric(counters=[], gauges=[]):
-    common.send_metric(counters, gauges)
+    signalfx_serverless_common.send_metric(counters, gauges)
 
 
 # convenience method
 def send_counter(metric_name, metric_value=1, dimensions={}):
-    common.send_counter(metric_name, metric_value, dimensions)
+    signalfx_serverless_common.send_counter(metric_name, metric_value, dimensions)
 
 
 # convenience method
 def send_gauge(metric_name, metric_value, dimensions={}):
-    common.send_gauge(metric_name, metric_value, dimensions)
+    signalfx_serverless_common.send_gauge(metric_name, metric_value, dimensions)
 
