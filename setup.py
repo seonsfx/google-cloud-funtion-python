91f55698def27866c1a5810ada382ad3c10acc80
Seon Kim <skim@signalfx.com>
2019-04-02 14:12:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ with open('signalfx_gcf/version.py') as f:
 with open('README.rst') as readme:
     long_description = readme.read()
 
-with open('requirements.txt') as f:
-    requirements = [line.strip() for line in f.readlines()]
+# with open('requirements.txt') as f:
+#     requirements = [line.strip() for line in f.readlines()]
 
 setup(
     name=name,  # noqa
@@ -24,7 +24,10 @@ setup(
     long_description_content_type='text/x-rst',
     zip_safe=True,
     packages=find_packages(),
-    install_requires=requirements,
+    # install_requires=requirements,
+    install_requires=[
+        "git+git://github.com/seonsfx/serverless-common-python@5a90b53c179a8043487f1a8733434dd3a3c958d3#egg=signalfx_serverless_common",
+    ],
     classifiers=[
         'Operating System :: OS Independent',
         'Programming Language :: Python',
